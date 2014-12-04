141de67e3757e11633e67e690c166c497cdb4944
Doge Mob <doge@incomm.com>
2014-12-04 11:45:34 -0800

    Chat functions done

--- src/index.js
+++ src/index.js
@@ -1,15 +1,41 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var state = require('./app/state');
+var User = require('./app/User');
+var userMap = {};
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/client/index.html');
 });
 
-io.on('connection', function(socket){
+app.get('/app.js', function (req, res) {
+  res.sendFile(__dirname + '/client/app.js');
+});
+
+io.on('connection', function (socket) {
   console.log('a user connected');
+  socket.emit('authenticate');
+  socket.on('login', function (name) {
+    if (name) {
+      var newUser = new User({
+        name: name
+      });
+      io.emit('chat message', newUser.name + " is logged in");
+      userMap[socket.id] = newUser.name;
+      state.users[name] = newUser;
+    }
+  });
+  socket.on('disconnect', function () {
+    delete userMap[socket.id];
+    console.log('user disconnected');
+  });
+  socket.on('chat message', function (msg) {
+    console.log('message: ' + msg);
+    io.emit('chat message', userMap[socket.id] + ': ' + msg);
+  });
 });
 
-http.listen(3000, function(){
+http.listen(3000, function () {
   console.log('listening on *:3000');
 });
